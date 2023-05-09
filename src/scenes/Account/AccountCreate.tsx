6afe986d9c31b1f80beaa333947609c0c86268ca
Sigit Haryoyudanto <hello@sigit.co.uk>
2023-05-09 18:26:18 +0000

    Sepolia

--- src/scenes/Account/AccountCreate.tsx
+++ src/scenes/Account/AccountCreate.tsx
@@ -67,7 +67,7 @@ function AccountCreate() {
   return (
     <div className='AccountCreate p-5 m-3 card shadow'>
       <h1>
-        Aqua Wallet [Sepolia Testnet]
+        Aqua Wallet [Sepolia]
       </h1>
       <form onSubmit={event => event.preventDefault()}>
         <button type="button" className="btn btn-primary" onClick={createAccount}>
